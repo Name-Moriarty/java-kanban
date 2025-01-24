6e3825572a8fc288b8f6f99f7677c5f8dd0cee03
Dvoeglazov Pavel <dvoeglazov.official@yandex.ru>
2025-01-24 21:16:53 +0500

    Выполненый 6 спринт без тестов.

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -3,23 +3,62 @@ package history;
 import task.Task;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int MAX_HISTORY_SIZE = 10;
-    private final List<Task> taskHistory = new ArrayList<>();
+    private Node <Task> head;
+    private Node <Task> tail;
+    private final Map <Integer,Node <Task>> taskHistory = new LinkedHashMap<>();
 
     @Override
     public void add(Task task) {
-        if (taskHistory.size() == MAX_HISTORY_SIZE) {
-            taskHistory.remove(0);
+        if (taskHistory.size() == 0){
+            tail= new Node<>(null,task,null);
+            taskHistory.put(task.getId(),tail);
+            return;
         }
-        taskHistory.add(task);
+        if (taskHistory.containsKey(task.getId())) {
+            remove(task.getId());
+        }
+        Node<Task> oldTail = tail;
+        Node<Task> newNode = new Node<>(oldTail, task, null);
+        tail = newNode;
+        taskHistory.put(task.getId(), tail);
+        if (oldTail == null)
+            head = newNode;
+        else
+            oldTail.setNext(newNode);
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(taskHistory);
+        ArrayList <Task> newList = new ArrayList<>();
+        for (Node <Task> taskNode: taskHistory.values()){
+            newList.add(taskNode.getData());
+        }
+        return newList;
+    }
+
+    @Override
+    public void remove (int id){
+        removeNode(taskHistory.get(id));
+        taskHistory.remove(id);
+    }
+
+    public void removeNode(Node<Task> node) {
+        if (node.getPrev() != null) {
+            node.getPrev().setNext(node.getNext());
+        } else {
+            head = node.getNext();
+        }
+
+        if (node.getNext() != null) {
+            node.getNext().setPrev(node.getPrev());
+        } else {
+            tail = node.getPrev();
+        }
     }
 
 }
